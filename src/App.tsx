a4c374d7138b6c32812b0adc2398e8800729c687
Vinicius Alexander Moreira Nascimento <vinicius.malex2002@gmail.com>
2025-05-20 19:36:35 -0300

    feat: implement slider mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import PopoverDemo from "./components/Popover";
+import SliderDemo from "./components/slider";
 import SwitchDemo from "./components/Switch";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <PopoverDemo />
       <SwitchDemo labelText="Opção 1" id="opção 1" />
       <SwitchDemo labelText="Opção 2" id="opção 2" />
+      <SliderDemo />
     </>
   );
 }
